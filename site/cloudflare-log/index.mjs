760421f3ad0118f628cca20ef8f1095b6b5334d4
ssc19193 <28866337+ssc19193@users.noreply.github.com>
2024-07-05 20:03:41 +0800

    fea 直接查询log

--- site/cloudflare-log/index.mjs
+++ site/cloudflare-log/index.mjs
@@ -1,19 +1,30 @@
+import {getDir} from '../tool.mjs'
+import path from 'path'
 import utc from 'dayjs/plugin/utc.js'
 import timezone from 'dayjs/plugin/timezone.js'
 import dayjs from 'dayjs'
 import express from 'express'
 
+let __dirname = getDir(import.meta.url)
+
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-let listenUri = process.env['URI_CFLOG'] || 'cloudflare-log'
+let listenUri = process.env['URI_CFLOG'] || '/cloudflare-log'
+let logCache = [];
 
 function register(app){
     console.log('register listen uri: '+ listenUri)
+    app.use(listenUri, express.static(path.resolve(__dirname, 'page')))
     app.use(listenUri, express.text())
-    app.post(listenUri, function(req,res){
-        console.log(req.body);
+
+    app.post(listenUri+'/push', function(req,res){
+        logCache.push(dayjs().tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')+"> "+req.body)
         res.end();
+    });
+    app.get(listenUri+'/pull', function(req,res){
+        res.send(JSON.stringify(logCache))
+        logCache=[];
     });
 }
 export default register
