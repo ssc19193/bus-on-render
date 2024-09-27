1a4f0e6802b11dfa737dbbcc8aa130031766b769
ssc19193 <28866337+ssc19193@users.noreply.github.com>
2024-09-27 18:41:28 +0800

    fea 添加sless;

--- app.mjs
+++ app.mjs
@@ -1,18 +1,28 @@
+import http from 'http'
 import express from 'express'
 import path from 'path'
 import hk1_app from './site/hk1-app/index.mjs'
 import cloudFlareLog from './site/cloudflare-log/index.mjs'
+import wssServer from './sless/server-sless.mjs'
 
-const app = new express();
+const uuid = process.env.UUID;
 const port = process.env.APP_PORT || 80;
+const app = express();
 
 hk1_app(app)
 cloudFlareLog(app)
 
-console.log('using port:'+port)
-app.listen(port, ()=>{
-    console.log('server listening in http://127.0.0.1:'+port)
-})
 app.get('/favicon.ico', function(req,res){
     res.sendFile(path.resolve('.', 'site/favicon.ico'))
 })
+
+const server = http.createServer(app);
+console.log('using port:'+port)
+server.listen(port);
+
+wssServer(server, uuid);
+
+server.on('error', console.log);
+server.on('listening', ()=>{
+    console.log('listening on:' + server.address().port)
+});
