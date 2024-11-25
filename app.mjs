32fbdcefdbbf67f795223cf952ca86f3f439e659
ssc19193 <28866337+ssc19193@users.noreply.github.com>
2024-11-25 17:40:53 +0800

    fea 添加animefilter;

--- app.mjs
+++ app.mjs
@@ -3,6 +3,7 @@ import express from 'express'
 import path from 'path'
 import hk1_app from './site/hk1-app/index.mjs'
 import cloudFlareLog from './site/cloudflare-log/index.mjs'
+import animefilter from './site/animefilter/index.mjs'
 import wssServer from './sless/server-sless.mjs'
 
 const uuid = process.env.UUID;
@@ -11,6 +12,11 @@ const app = express();
 
 hk1_app(app)
 cloudFlareLog(app)
+animefilter(app)
+
+app.get('/', function(req,res){
+    res.sendStatus(200)
+})
 
 app.get('/favicon.ico', function(req,res){
     res.sendFile(path.resolve('.', 'site/favicon.ico'))
